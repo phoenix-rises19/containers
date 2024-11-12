081f1551ea0095898e720a7ab151f17376d38117
phoenix-rises19 <manocha.vaibhav19@gmail.com>
2024-11-12 08:52:07 +0530

    Update List.hpp

--- List.hpp
+++ List.hpp
@@ -1,6 +1,7 @@
 #include <stddef.h>
-#ifndef list.hpp
-#define list.hpp
+#include<iostream>
+#ifndef LIST_HPP
+#define LIST_HPP
 
 
 
@@ -9,35 +10,50 @@ namespace vai{
     template<typename T>
     class List{
 
+    private:
+
+        struct ListNode{
+            T value;
+            ListNode* next;
+            ListNode* previous;
+
+            ListNode(T& val):
+                value(val),
+                next(nullptr),
+                previous(nullptr){}
+
+            ListNode():
+                value(T()),
+                next(nullptr),
+                previous(nullptr){}
+
+            ListNode(ListNode* oth):
+                value(oth ? oth->value : 0),
+                next(oth ? oth->next : nullptr),
+                previous(oth ? oth->previous : nullptr){}
+
+            template <class... Args>
+            ListNode(Args&&... args) : value(std::forward<Args>(args)...) {}
+
+            ~ListNode(){}
+        };
+
+        ListNode* head;
+        ListNode* tail;
+        size_t size_;
+
     public:
         List():
-            head(nullptr)
-            tail(nullptr)
+            head(nullptr),
+            tail(nullptr),
             size_(0){}
         
 
 
         List& operator=(List& other){
-            if(this==&other){
+            if(this==&other) {
                 return *this;
-        }
-        
-        bool operator==(const List& other) const{
-            if(this->size_!=other.size_){
-                return false;
             }
-            ListNode* current=this->head;
-            ListNode* otherCurrent=other.head;
-            while(current and otherCurrent){
-                if(current->value!=otherCurrent.value){
-                    return false;
-                }
-                current=current->next;
-                otherCurrent=otherCurrent->next;
-            }
-            return true;
-        }
-
             clear();
 
             size_=other.size_;
@@ -68,15 +84,31 @@ namespace vai{
             size_=other.size_;
             head=other.head;
             tail=other.tail;
-            other.size=0;
+            other.size_=0;
             other.head=nullptr;
             other.tail=nullptr;
             return *this;
         }
+
+        bool operator==( List& other){
+            if(this->size_!=other.size_){
+                return false;
+            }
+            ListNode* current=this->head;
+            ListNode* otherCurrent=other.head;
+            while(current and otherCurrent){
+                if(current->value!=otherCurrent->value){
+                    return false;
+                }
+                current=current->next;
+                otherCurrent=otherCurrent->next;
+            }
+            return true;
+        }
         
         class Iterator{
             public:
-                Iterator(ListNode* node)
+                Iterator(ListNode* node):
                     current(node){}
 
                 T& operator*(){
@@ -105,9 +137,20 @@ namespace vai{
                     return current;
                 }
 
+                bool operator==(const Iterator& other) {
+                    if(this->current==other.current){
+                        return true;
+                    }
+                    return false;
+                }
+
+                bool operator!=(const Iterator& other){
+                    return !(*this==other);
+                }
+
 
             private:
-                ListNode* current;
+               typename List<T>:: ListNode* current;
             
         };
         
@@ -125,7 +168,109 @@ namespace vai{
             ++size_;
         }
 
-        
+        template< class... Args>
+        Iterator emplace(Iterator iter, Args&&... args){
+            ListNode* node=iter.getNode();
+            ListNode* newNode=new ListNode(std::forward<Args>(args)...);
+            ++size_;
+            if(node==head){
+                newNode->next=node;
+                node->previous=newNode;
+                head=newNode;
+                return Iterator(head);
+            }
+            else if(node== nullptr){
+                if(tail){
+                    tail->next=newNode;
+                    newNode->previous=tail;
+                }
+                tail=newNode;
+                if(head==nullptr){
+                    head=tail;
+                }
+                return Iterator(tail);
+            }
+            else{
+                ListNode* temp=node->previous;
+                temp->next=newNode;
+                newNode->previous=temp;
+                newNode->next=node;
+                node->previous=newNode;
+                return Iterator(newNode);
+            }
+        }
+
+        template<class ... Args>
+        void emplace_back(Args&&... args){
+            ListNode* node= new ListNode(std::forward<Args>(args)...);
+            if(head==nullptr){
+                head=node;
+                tail=node;
+            }
+            else{
+                tail->next=node;
+                node->previous=tail;
+                tail=node;
+            }
+            ++size_;
+        }
+
+        template<class ... Args>
+        void emplace_front(Args&&... args){
+            ListNode* node= new ListNode(std::forward<Args>(args)...);
+            if(head==nullptr){
+                head=node;
+                tail=node;
+           }
+           else{
+               node->next=head;
+               head->previous=node;
+               head=node;
+           }
+           ++size_;
+        }
+
+        void push_front(T& value){
+            ListNode* node= new ListNode(value);
+            if(head==nullptr){
+                head=node;
+                tail=node;
+            }
+            else{
+                node->next=head;
+                head->previous=node;
+                head=node;
+            }
+            ++size_;
+        }
+
+        void pop_back(){
+            if (tail == nullptr) return;
+            if (head == tail) {
+                delete tail;
+                head = tail = nullptr;
+            } else {
+                ListNode* temp = tail;
+                tail = tail->previous;
+                tail->next = nullptr;
+                delete temp;
+            }
+            --size_;
+        }
+
+        void pop_front(){
+            if (head == nullptr) return;
+            if (head == tail) {
+                delete head;
+                head = tail = nullptr;
+            } else {
+                ListNode* temp = head;
+                head = head->next;
+                head->previous = nullptr;
+                delete temp;
+            }
+            --size_;
+        }
 
         ListNode* front(){
             if(head==nullptr){
@@ -190,10 +335,10 @@ namespace vai{
             return Iterator(new_node);
         }
 
-        Iterator& begin(){
+        Iterator begin(){
             return Iterator(head);
         }
-        Iterator& end(){
+        Iterator end(){
             return Iterator(nullptr);
         }
 
@@ -205,35 +350,79 @@ namespace vai{
             return Iterator(head);
         }
 
-    private:
-
-        struct ListNode{
-            T value;
-            ListNode* next;
-            ListNode* previous;
-
-            ListNode(T& val):
-                value(val),
-                next(nullptr),
-                previous(nullptr){}
-            
-            ListNode():
-                value(0),
-                next(nullptr),
-                previous(nullptr){}
-
-            ListNode(ListNode* oth):
-                value(oth ? oth->value : 0),            
-                next(oth ? oth->next : nullptr),        
-                previous(oth ? oth->previous : nullptr){}
-
-            ~ListNode(){}
+        void erase(Iterator pos){
+            ListNode* node=pos.getNode();
+            if(node==head){
+                head=head->next;
+                head->previous=nullptr;
+                delete node;
+            }
+            else if(node==tail){
+                tail=tail->previous;
+                tail->next=nullptr;
+                delete node;
+            }
+            else{
+                ListNode* temp=node->previous;
+                temp->next=node->next;
+                node->next->previous=temp;
+                delete node;
+            }
+            --size_;
         }
-        ListNode<T>* head;
-        ListNode<T>* tail;
-        size_t size_;
 
+        Iterator find(T& value){
+            for(auto iter=begin();iter!=end();++iter){
+                if(iter->value == value){
+                    return iter;
+                }
+            }
+            return Iterator(end);
+        }
 
+        void reverse(){
+            ListNode* current = head;
+            ListNode* temp = nullptr;
+            while (current != nullptr) {
+                temp = current->previous;
+                current->previous = current->next;
+                current->next = temp;
+                current = current->previous;
+            }
+            if (temp != nullptr) {
+                tail = head;
+                head = temp->previous;
+            }
+        }
+        void merge(List & other){
+            if(this==&other){
+                return;
+            }
+            ListNode* thisCurrent = head;
+            ListNode* otherCurrent = other.head;
+            List<T> result;
+
+            while (thisCurrent && otherCurrent) {
+                if (thisCurrent->value < otherCurrent->value) {
+                    result.push_back(thisCurrent->value);
+                    thisCurrent = thisCurrent->next;
+                } else {
+                    result.push_back(otherCurrent->value);
+                    otherCurrent = otherCurrent->next;
+                }
+            }
+            while (thisCurrent) {
+                result.push_back(thisCurrent->value);
+                thisCurrent = thisCurrent->next;
+            }
+            while (otherCurrent) {
+                result.push_back(otherCurrent->value);
+                otherCurrent = otherCurrent->next;
+            }
+
+            *this = std::move(result);
+            other.clear();
+        }
 };
 
  
